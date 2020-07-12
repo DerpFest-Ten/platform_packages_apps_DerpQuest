DerpFest-AOSP/packages_apps_DerpQuest

DerpQuest: Add HeadsUp timeout function [2/2]
Forward ported to marshmallow/nougat By: @BeansTown106
removed the none option as we now have a headsup minimum time of 2seconds
added 5sec option as that is the new default in marshmallow

src/com/derpquest/settings/fragments/HeadsUp.java
@@ -58,8 +58,10 @@ public class HeadsUp extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, Indexable {
 
     private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
 
     private ListPreference mHeadsUpSnoozeTime;
+    private ListPreference mHeadsUpTimeOut;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -84,6 +86,15 @@ public class HeadsUp extends SettingsPreferenceFragment implements
                 Settings.System.HEADS_UP_NOTIFICATION_SNOOZE, defaultSnooze);
         mHeadsUpSnoozeTime.setValue(String.valueOf(headsUpSnooze));
         updateHeadsUpSnoozeTimeSummary(headsUpSnooze);
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
     }
 
     @Override
@@ -93,6 +104,12 @@ public class HeadsUp extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                     Settings.System.HEADS_UP_NOTIFICATION_SNOOZE, headsUpSnooze);
             updateHeadsUpSnoozeTimeSummary(headsUpSnooze);
+            return true;
+        } else if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT, headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
             return true;
         }
         return false;
@@ -107,6 +124,12 @@ public class HeadsUp extends SettingsPreferenceFragment implements
             String summary = getResources().getString(R.string.heads_up_snooze_summary, value / 60 / 1000);
             mHeadsUpSnoozeTime.setSummary(summary);
         }
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        mHeadsUpTimeOut.setSummary(summary);
     }
 
     @Override

DerpFest-Ten/platform_packages_apps_DerpQuest

Change-Id: Id2a17f307801c0a56956d7b06191c50a3ca9c47a

src/com/derpquest/settings/notification/HeadsUp.java
@@ -40,8 +40,10 @@ public class HeadsUp extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
     private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
 
     private ListPreference mHeadsUpSnoozeTime;
+    private ListPreference mHeadsUpTimeOut;
 
     @Override
     public int getMetricsCategory() {
@@ -71,6 +73,15 @@ public class HeadsUp extends SettingsPreferenceFragment implements
         mHeadsUpSnoozeTime.setValue(String.valueOf(headsUpSnooze));
         updateHeadsUpSnoozeTimeSummary(headsUpSnooze);
 
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
+
       }
 
     @Override
@@ -86,6 +97,12 @@ public class HeadsUp extends SettingsPreferenceFragment implements
                     headsUpSnooze);
             updateHeadsUpSnoozeTimeSummary(headsUpSnooze);
             return true;
+        } else if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT, headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
         }
        return false;
     }
@@ -99,6 +116,12 @@ public class HeadsUp extends SettingsPreferenceFragment implements
             String summary = getResources().getString(R.string.heads_up_snooze_summary, value / 60 / 1000);
             mHeadsUpSnoozeTime.setSummary(summary);
         }
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        mHeadsUpTimeOut.setSummary(summary);
     }
 
     /**